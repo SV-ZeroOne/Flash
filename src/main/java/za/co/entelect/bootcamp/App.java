13677665c7b5a8eb5d5c3000c2e42f14075fb275
Steve Velcev <steve.velcev@entelect.co.za>
2017-01-11 14:19:29 +0200

    Bug Fix

--- src/main/java/za/co/entelect/bootcamp/App.java
+++ src/main/java/za/co/entelect/bootcamp/App.java
@@ -4,7 +4,7 @@ public class App
 {
     public static final double CURRENT_RATE = 22.07;
 
-    public float convertRandToDollar(double value){
+    public double convertRandToDollar(double value){
         return value * CURRENT_RATE / 100 ;
     }
 
