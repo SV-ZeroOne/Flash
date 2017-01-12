535ef6361977e1c9ba0ea65ed9780dcb4e474def
Steve Velcev <steve.velcev@entelect.co.za>
2017-01-12 10:59:30 +0200

    Added player attributes

--- src/main/java/za/co/entelect/bootcamp/Player.java
+++ src/main/java/za/co/entelect/bootcamp/Player.java
@@ -1,9 +1,23 @@
 package za.co.entelect.bootcamp;
 
-/**
- * @author kevin.gouws
- *
- * Created on 2017/01/12.
- */
 public class Player {
+
+    private String playerName;
+    private String weaponChoice;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getWeaponChoice() {
+        return weaponChoice;
+    }
+
+    public void setWeaponChoice(String weaponChoice) {
+        this.weaponChoice = weaponChoice;
+    }
 }
