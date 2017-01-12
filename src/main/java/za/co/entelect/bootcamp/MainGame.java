fe2dc8075ecc826553e1642ac5e5eaa2f2058680
kevin.gouws <st1Xz_Unl3ash3D_aB>
2017-01-12 10:31:42 +0200

    Created Roshambo game structure.

---
+++ src/main/java/za/co/entelect/bootcamp/MainGame.java
@@ -0,0 +1,12 @@
+package za.co.entelect.bootcamp;
+
+/**
+ * @author kevin.gouws
+ *
+ * Created on 2017/01/12.
+ */
+public class MainGame {
+    public static void main(String[] args) {
+        System.out.println("This will be the main class.");
+    }
+}
