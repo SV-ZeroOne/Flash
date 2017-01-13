b1c9738d0f17cc3864875ef9830234eb418d7523
Byron Dinkelmann <byron.dinkelmann@entelect.co.za>
2017-01-13 16:44:38 +0200

    added Stock and Supplier classes from ERD

--- domain/src/main/java/za/co/entelect/bootcamp/flash/Stock.java
+++ domain/src/main/java/za/co/entelect/bootcamp/flash/Stock.java
@@ -1,8 +1,64 @@
 package za.co.entelect.bootcamp.flash;
 
 /**
- * Created by kevin.gouws on 2017/01/13.
+ * Created by byron.dinkelmann on 2017/01/13.
  */
 public class Stock {
-    public Stock() {}
+
+    private int stockReferenceID;
+    private int issueID;
+    private String condition;
+    private short availableQty;
+    private float price;
+
+    public Stock(){}
+
+    public Stock(int stockReferenceID,int issueID,String condition,short availableQty, float price)
+    {
+        this.stockReferenceID=stockReferenceID;
+        this.issueID=issueID;
+        this.condition=condition;
+        this.availableQty=availableQty;
+        this.price=price;
+    }
+
+    public int getStockReferenceID() {
+        return stockReferenceID;
+    }
+
+    public void setStockReferenceID(int stockReferenceID) {
+        this.stockReferenceID = stockReferenceID;
+    }
+
+    public int getIssueID() {
+        return issueID;
+    }
+
+    public void setIssueID(int issueID) {
+        this.issueID = issueID;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public short getAvailableQty() {
+        return availableQty;
+    }
+
+    public void setAvailableQty(short availableQty) {
+        this.availableQty = availableQty;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
