b2b564785745dee5b4148e4f9079a6760b15b693
Kevin Gouws <kevin.gouws@entelect.co.za>
2017-01-13 15:34:32 +0200

    Expanded on the project structure

--- app/src/main/java/za/co/entelect/bootcamp/flash/Main.java
+++ app/src/main/java/za/co/entelect/bootcamp/flash/Main.java
@@ -1,3 +1,5 @@
+package za.co.entelect.bootcamp.flash;
+
 /**
  * Created by kevin.gouws on 2017/01/13.
  */
---
+++ app/src/test/java/za/co/entelect/bootcamp/flash/PlaceholderClassOne.java
@@ -0,0 +1,7 @@
+package za.co.entelect.bootcamp.flash;
+
+/**
+ * Created by kevin.gouws on 2017/01/13.
+ */
+public class PlaceholderClassOne {
+}
