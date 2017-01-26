9a051758a279b007341b324e6385ccf3351c32e0
kevin.gouws <kevin.gouws@entelect.co.za>
2017-01-26 19:10:41 +0200

    Added snackbar

--- web/src/main/js/product.js
+++ web/src/main/js/product.js
@@ -73,6 +73,12 @@ function addToCart(){
         html += "</span>";
         html += "</li>";
         $(".shoppingCartDropDown").append(html);
+        // Get the snackbar DIV
+        var x = document.getElementById("snackbar")
+        // Add the "show" class to DIV
+        x.className = "show";
+        // After 3 seconds, remove the show class from DIV
+        setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
     }
 }
 
