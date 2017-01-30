3bd6440308a92f296e3df961530f725c825b9398
kevin.gouws <kevin.gouws@entelect.co.za>
2017-01-30 17:17:22 +0200

    Completed Basic practical for Spring Web MVC

--- web/src/main/java/za/co/entelect/bootcamp/flash/web/config/WebConfig.java
+++ web/src/main/java/za/co/entelect/bootcamp/flash/web/config/WebConfig.java
@@ -1,7 +1,16 @@
 package za.co.entelect.bootcamp.flash.web.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 /**
  * @author kevin.gouws - Created on 2017/01/30.
  */
+
+@Configuration
+@ComponentScan(basePackages = "za.co.entelect.bootcamp.flash.web.controllers")
 public class WebConfig {
+
 }
--- web/src/main/java/za/co/entelect/bootcamp/flash/web/config/WebInitializer.java
+++ web/src/main/java/za/co/entelect/bootcamp/flash/web/config/WebInitializer.java
@@ -1,7 +1,21 @@
 package za.co.entelect.bootcamp.flash.web.config;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author kevin.gouws - Created on 2017/01/30.
  */
-public class WebInitializer {
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[] {ServicesRootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[] {WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[] {"/*"};
+    }
 }
