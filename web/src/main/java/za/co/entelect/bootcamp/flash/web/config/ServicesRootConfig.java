3cc774545968b4867afdbee06de206ac6e73b45c
kevin.gouws <kevin.gouws@entelect.co.za>
2017-01-30 16:18:15 +0200

    Corrected incorrect project structure - Thanks Matt!

--- web/src/main/java/za/co/entelect/bootcamp/flash/web/config/ServicesRootConfig.java
+++ web/src/main/java/za/co/entelect/bootcamp/flash/web/config/ServicesRootConfig.java
@@ -1,7 +1,13 @@
 package za.co.entelect.bootcamp.flash.web.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
 /**
  * @author kevin.gouws - Created on 2017/01/30.
  */
+
+@Configuration
+@ImportResource("classpath:services-configuration.xml")
 public class ServicesRootConfig {
 }
