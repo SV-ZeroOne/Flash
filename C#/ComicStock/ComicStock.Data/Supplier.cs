dbdc0b76294ce09589caccdafe34526dd5ea4ff0
byron.dinkelmann <byron.dinkelmann@entelect.co.za>
2017-02-07 18:12:54 +0200

    added new database test

---
+++ C#/ComicStock/ComicStock.Data/Supplier.cs
@@ -0,0 +1,35 @@
+namespace ComicStock.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Supplier
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Supplier()
+        {
+            Orders = new HashSet<Order>();
+            SupplierQuotes = new HashSet<SupplierQuote>();
+        }
+
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(25)]
+        public string ReferenceNumber { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<SupplierQuote> SupplierQuotes { get; set; }
+    }
+}
