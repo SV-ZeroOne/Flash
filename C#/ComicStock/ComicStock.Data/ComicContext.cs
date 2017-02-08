a442fa0db989ed16254165b02b0a7bb1fe2ffe46
quinton.weenink <quinton.weenink@entelect.co.za>
2017-02-08 11:16:44 +0200

    Added to context configurations

--- C#/ComicStock/ComicStock.Data/ComicContext.cs
+++ C#/ComicStock/ComicStock.Data/ComicContext.cs
@@ -31,22 +31,7 @@ namespace ComicStock.Data
 
             modelBuilder.Configurations.Add(new IssueConfiguration());
 
-            modelBuilder.Entity<Creator>()
-                .Property(e => e.Name)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Creator>()
-                .Property(e => e.CountryOfResidence)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Creator>()
-                .Property(e => e.EmailAddress)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Creator>()
-                .HasMany(e => e.ComicCreators)
-                .WithRequired(e => e.Creator)
-                .WillCascadeOnDelete(false);
+            modelBuilder.Configurations.Add(new CreatorConfiguration());
 
             modelBuilder.Entity<Order>()
                 .Property(e => e.Total)
@@ -78,32 +63,9 @@ namespace ComicStock.Data
                 .Property(e => e.Total)
                 .HasPrecision(8, 2);
 
-            modelBuilder.Entity<SupplierQuote>()
-                .Property(e => e.Price)
-                .HasPrecision(8, 2);
+            modelBuilder.Configurations.Add(new SupplierQuoteConfiguration());
 
-            modelBuilder.Entity<SupplierQuote>()
-                .HasMany(e => e.IssueOrders)
-                .WithRequired(e => e.SupplierQuote)
-                .HasForeignKey(e => e.QuoteID)
-                .WillCascadeOnDelete(false);
-
-            modelBuilder.Entity<Supplier>()
-                .Property(e => e.Name)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Supplier>()
-                .Property(e => e.City)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Supplier>()
-                .Property(e => e.ReferenceNumber)
-                .IsUnicode(false);
-
-            modelBuilder.Entity<Supplier>()
-                .HasMany(e => e.SupplierQuotes)
-                .WithRequired(e => e.Supplier)
-                .WillCascadeOnDelete(false);
+            modelBuilder.Configurations.Add(new SupplierConfiguration());
 
             modelBuilder.Entity<Voucher>()
                 .Property(e => e.Code)
