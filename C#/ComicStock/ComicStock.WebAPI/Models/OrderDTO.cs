0acf1a81ad93b79d75a24e59e146960b9b962a20
quinton.weenink <quinton.weenink@entelect.co.za>
2017-02-09 12:52:26 +0200

    Added constructor to the OrderDTO

--- C#/ComicStock/ComicStock.WebAPI/Models/OrderDTO.cs
+++ C#/ComicStock/ComicStock.WebAPI/Models/OrderDTO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComicStock.Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,19 +8,36 @@ namespace ComicStock.WebAPI.Models
 {
     public class OrderDTO
     {
-        /* public int Id { get; set; }
-         public Data orderDate { get; set; }
-         public string Country { get; set; }
-         public byte[] TaxRef { get; set; }
-         public string Email { get; set; }
+        public int Id { get; set; }
+        public DateTime? OrderDate { get; set; }
+        public decimal? Total { get; set; }
+        public string ShipmentRef { get; set; }
+        public DateTime? ShipmentDate { get; set; }
+        public string DeliveryStatus { get; set; }
+        public Supplier Supplier { get; set; }
 
-         private OrderDTO() { }
+        private OrderDTO() { }
 
         public OrderDTO(Order order)
         {
-            
-          
+            this.Id = order.ID;
+            this.OrderDate = order.OrderDate;
+            this.Total = order.Total;
+            this.ShipmentRef = order.ShipmentRef;
+            this.ShipmentDate = order.ShipmentDate;
+            this.DeliveryStatus = order.DeliveryStatus;
         }
-         */
+
+        public Order CreateDomainObject(Order order)
+        {
+            order.ID = this.Id;
+            order.OrderDate = this.OrderDate;
+            order.Total = this.Total;
+            order.ShipmentRef = this.ShipmentRef;
+            order.ShipmentDate = this.ShipmentDate;
+            order.DeliveryStatus = this.DeliveryStatus;
+
+            return order;
+        }
     }
 }
