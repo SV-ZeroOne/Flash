9563d40b8892a223c199a9e1005101b3d8d41355
Quinton Weenink <quinton.weenink@entelect.co.za>
2017-02-12 11:36:06 +0200

    Awesome generic exception with status

---
+++ C#/ComicStock/ComicStock.API/Exceptions/HttpException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicStock.API.Exceptions
+{
+    public class HttpException : Exception
+    {
+        public HttpStatusCode httpStatusCode = HttpStatusCode.NotFound;
+    }
+}
