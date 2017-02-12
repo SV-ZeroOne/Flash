cef537c5877370dfbe23e2750cc99591b7797d1e
Chris Van De Sande <chris.vdsande@entelect.co.za>
2017-02-12 11:23:22 +0200

    added a frontend for Angular app

---
+++ C#/ComicStock/ComicStock.WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using ComicStock.WebAPI;
+
+
+namespace ComicStock.WebAPI.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
