ec9c3dfc68a472b37b6832a26dba4f015f6d4bf4
Chris Van De Sande <chris.vdsande@entelect.co.za>
2017-02-13 13:37:57 +0200

    fixed supplier.html added font awesome

--- C#/ComicStock/ComicStock.WebAPI/app/supplier/supplier.module.js
+++ C#/ComicStock/ComicStock.WebAPI/app/supplier/supplier.module.js
@@ -11,3 +11,31 @@ angular
 		},
 		template: "<ol><li>{{$ctrl.message}}</li></ol>"
 	});
+
+
+
+
+//angular.module('admin.ResourceManager')
+//   .service('SupplierResource', function ($http) {
+//       var $supplierResource = this;
+
+//       $supplierResource.getAll = function ($ctrl, filter) {
+//           if (!filter) {
+//               filter = {};
+//           }
+
+//           $http
+//               .get('http://admin.square-eyes.co.za:1918/api/Supplier', {
+//                   params: filter
+//               })
+//               .then(function (response) {
+//                   $ctrl.suppliers = response.data;
+//               })
+//               .catch(function (data, status) {
+//                   alert("Error");
+//               });
+//       };
+
+//       $supplierResource.get = function (id) {
+//           console.log("supplierResource - Get called " + id);
+//       };
