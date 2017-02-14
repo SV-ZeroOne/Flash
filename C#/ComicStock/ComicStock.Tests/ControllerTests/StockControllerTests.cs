2370ca1b92a8303788fa7260b98f3640ce353939
byron.dinkelmann <byron.dinkelmann@entelect.co.za>
2017-02-14 15:27:46 +0200

    fixed comments

--- C#/ComicStock/ComicStock.Tests/ControllerTests/StockControllerTests.cs
+++ C#/ComicStock/ComicStock.Tests/ControllerTests/StockControllerTests.cs
@@ -45,48 +45,6 @@ namespace ComicStock.Tests.ControllerTests
 
         }
 
-        /*[Test]
-        public void PostReturnsStockIDAddRepository()
-        {
-            Stock testStock =
-            new Stock
-            {
-                IssueID = null,
-                Condition = "testCon",
-                AvailableQty = Int16.Parse("5"),
-                Price = 100,
-            };
-
-            StockDTO testStockDTO = new StockDTO(testStock);
-
-            IHttpActionResult actionResult = stockController.Post(testStockDTO);
-            var contentResult = actionResult as OkNegotiatedContentResult<int>;
-
-            int testID = contentResult.Content;
-            Assert.IsNotNull(contentResult.Content);
-
-            /*testIssueDTO.Title = "issue put";
-            testIssueDTO.Publisher = "test publisher put";
-            testIssueDTO.Description = "issue put";
-
-            IHttpActionResult actionResultPut = issuesController.Put(testID, testIssueDTO);
-            var contentResultPut = actionResultPut as OkNegotiatedContentResult<IssueDTO>;
-
-            Assert.IsNotNull(contentResultPut.Content);
-            Assert.AreEqual("issue put", contentResultPut.Content.Title);
-            Assert.AreEqual("test publisher put", contentResultPut.Content.Publisher);
-            Assert.AreEqual("issue put", contentResultPut.Content.Description);
-            
-
-            actionResult = stockController.Delete(testID);
-            Assert.IsNotNull(actionResult);
-
-            IHttpActionResult actionResultUpdate = stockController.Get(testID);
-            var contentResultUpdate = actionResultUpdate as OkNegotiatedContentResult<StockDTO>;
-
-            Assert.AreEqual(Int16.Parse("0"), contentResultUpdate.Content.AvailableQuantity);
-
-        }*/
         [Test]
         public void GetReturnsStockGetPage()
         {
