3eca91b980c7a4ad47b36ae8d7bcb2590911b302
Quinton Weenink <quinton.weenink@entelect.co.za>
2017-02-15 22:28:07 +0200

    Added the new repos

---
+++ C#/ComicStock/ComicStock.Data/IRepositories/ISupplierQuoteRepository.cs
@@ -0,0 +1,14 @@
+﻿using ComicStock.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicStock.Data.IRepositories
+{
+    public interface ISupplierQuoteRepository : IRepository<SupplierQuote>
+    {
+        IEnumerable<SupplierQuote> GetPage(string search, int page, int pageSize);
+    }
+}
